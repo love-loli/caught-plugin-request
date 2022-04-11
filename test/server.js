5d16fa915dc642cc1cfe6b66bf924ff29ba14a41
lbw <574524020@qq.com>
2022-04-11 15:49:50 +0800

    Add test server

---
+++ test/server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = 3000
+
+app.use(cors())
+// 200
+app.get('/200', (req, res) => {
+  res.status(200).json(JSON.stringify(req.params, null, 2))
+})
+app.post('/200', (req, res) => {
+  res.status(200).json(JSON.stringify(req.params, null, 2))
+})
+
+// 500
+app.get('/500', (_, res) => {
+  res.status(500).send('Internal Server Error')
+})
+
+// 401
+app.get('/401', (_, res) => {
+  res.status(401).send('Please login in')
+})
+
+app.listen(port, () => {
+  console.info('Test server has been started')
+})
